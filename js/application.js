6460cd6c9c94cb4f7236e2270309c7aa4b3e9253
Chris Pilko <chris.pilko@gmail.com>
2014-08-05 11:32:38 -0400

    Got partial working and send email via mailto: link.

--- js/application.js
+++ js/application.js
@@ -1,6 +1,18 @@
 window.IoM = Ember.Application.create();
 
-
 IoM.Router.map(function() {
 	this.resource('iom', { path: '/' });
+});
+
+IoM.IomController = Ember.Controller.extend({
+  name: "Chris",
+  
+  actions: {
+      sendToEmail: function(){
+	    var email_su = $("#subject-preview").text();
+            var email_body = $("#email-preview").text();
+            var email_link = "mailto:?subject=" + encodeURIComponent( $.trim(email_su) ) + "&body=" + encodeURIComponent( $.trim(email_body));
+            window.open (email_link, "_blank");
+      }
+  }
 });
