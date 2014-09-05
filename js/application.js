f078a6f8ac69287640b754bb6a71257edd304c32
Chris Pilko <chris.pilko@gmail.com>
2014-09-05 17:23:28 -0400

    Encoded some html entities in CFD.html

--- js/application.js
+++ js/application.js
@@ -4,15 +4,26 @@ IoM.Router.map(function() {
 	this.resource('iom', { path: '/' });
 });
 
+IoM.iomRoute = Ember.Route.extend({
+    model: function() {
+        return emails;
+    }
+});
+
 IoM.IomController = Ember.Controller.extend({
   name: "Chris",
   
   actions: {
       sendToEmail: function(){
-	    var email_su = $("#subject-preview").text();
+	    var email_su = this.email1.subject;
             var email_body = $("#email-preview").text();
             var email_link = "mailto:?subject=" + encodeURIComponent( $.trim(email_su) ) + "&body=" + encodeURIComponent( $.trim(email_body));
-            window.open (email_link, "_blank");
+            console.log(email_su);
+            //window.open (email_link, "_blank");
       }
   }
 });
+
+var emails = [{
+        subject: "Cub Scout Family Fun Day, Sunday 9/21 from 1-4pm @ Veteran’s Park Broomall"
+}];
